d4e6ddadf732d6e1a91d0576651cab8bbade55ea
emtech-user <emtech-user@PC-number>
2026-06-01 13:35:39 +0900

    sato

--- Freezer/src/main/java/model/dao/ConnectionManager.java
+++ Freezer/src/main/java/model/dao/ConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	//データベースURL
-		private static final String URL = "jdbc:mysql://localhost:3306/freezedb";
+		private static final String URL = "jdbc:mysql://localhost:3306/freezerdb";
 		
 		private static final String USER ="freezerU";
 		
