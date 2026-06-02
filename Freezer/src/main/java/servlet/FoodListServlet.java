22b813aa483074d11b7c39a94c1eb0c89dbcfdfe
emtech-user <emtech-user@PC-number>
2026-06-02 18:57:39 +0900

    Merge branch 'master' of https://github.com/niigata-aaa/freeza.git　佐藤

--- Freezer/src/main/java/servlet/FoodListServlet.java
+++ Freezer/src/main/java/servlet/FoodListServlet.java
@@ -34,6 +34,7 @@ public class FoodListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 		doPost(request, response);
 	}
 
